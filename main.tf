c225ec3e9b1be50e6f1d5a07ae9fa7ebd4c656f3
bledrunner <bledrunner@protonmail.com>
2020-09-20 22:39:48 +0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+data "aws_ami" "ubuntu-18-04" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # Canonical
+}
+
+data "aws_ami" "ubuntu-20-04" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # Canonical
+}
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "ubuntu-18-04" {
+  value = data.aws_ami.ubuntu-18-04
+}
+
+output "ubuntu-20-04" {
+  value = data.aws_ami.ubuntu-20-04
+}
